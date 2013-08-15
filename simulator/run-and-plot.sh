89aa5827abb9ba43d37e73af12b7fedc56151d59
Joe Desbonnet <jdesbonnet@gmail.com>
2013-08-15 18:11:54 +0100

    no comment

--- simulator/run-and-plot.sh
+++ simulator/run-and-plot.sh
@@ -2,6 +2,7 @@
 #
 # Run simulation and plot result with GNUPlot
 # Add PID parameters Kp Ki Kd as arguments
+# eg bash run-and-plot.sh 2 0.0001 500
 #
 ./water-pid-simulator $@ > t.dat
 gnuplot plot.gp
