b82fb6a5baeb191b3a17399c8d09a54bacb41d58
Joe Desbonnet <jdesbonnet@gmail.com>
2013-08-16 01:35:45 +0100

    run with bash run-and-plot.sh  -Kp 2 -Ki 0.0001 -Kd 500 -l 1 -ts 0 -te 3600

--- simulator/plot.gp
+++ simulator/plot.gp
@@ -1,2 +1,4 @@
-plot 't.dat' with linespoints
+plot 't.dat' with linespoints title "Water temperature °C", \
+	't.dat' using 1:5 with linespoints title "PID output", \
+	't.dat' using 1:($6 * 10) with lines title "Heating element"
 pause -1
--- simulator/water-pid-simulator.c
+++ simulator/water-pid-simulator.c
@@ -11,37 +11,89 @@
 // Specific heat capacity of water (J/kg/K)
 #define SHC_H2O (4186.0)
 
+typedef struct {
+	double t_start;
+	double t_end;
+	double set_point_temperature;
+	double dt;
+	double Kp;
+	double Ki;
+	double Kd;
+	double water_mass; // Mass of water in kg
+	double ambient_temperature; // Ambient temperature in °C
+	double sensor_time_constant; // Sensor temperature equalization tc
+} simulation_parameters_t;
 
 int main (int argc, char ** argv) {
 
+	int i;
+	simulation_parameters_t param;
+
+	// defaults
+	param.t_start=0.0;
+	param.t_end=7200.0;
+	param.set_point_temperature=60.0;
+	param.Kp = atol(argv[1]);
+	param.Ki = atol(argv[2]);
+	param.Kd = atol(argv[3]);
+	param.water_mass = 1.0;
+	param.ambient_temperature = 20.0;
+	param.sensor_time_constant = 1.0/300.0;
+
+	for (i = 1; i < argc; i++) {
+		if (strcmp("-ts",argv[i])==0) {
+			param.t_start = atol(argv[i+1]);
+			i++;
+		}
+		if (strcmp("-te",argv[i])==0) {
+			param.t_end = atol(argv[i+1]);
+			i++;
+		}
+
+		if (strcmp("-Kp",argv[i])==0) {
+			param.Kp = atol(argv[i+1]);
+			i++;
+		}
+		if (strcmp("-Ki",argv[i])==0) {
+			param.Ki = atol(argv[i+1]);
+			i++;
+		}
+		if (strcmp("-Kd",argv[i])==0) {
+			param.Kd = atol(argv[i+1]);
+			i++;
+		}
+		if (strcmp("-l",argv[i])==0) {
+			param.water_mass = atol(argv[i+1]);
+			i++;
+		}
+	}
+
+	run_simulation(param);
+}
+
+int run_simulation (simulation_parameters_t param) {
+
+	int i = 0;
 	double element_power = 400;
 	double dt = 0.1;
-	double Kp = 1;
-	double Ki = 0.001;
-	double Kd = 0.001;
 
 	double time;
-	double water_quantity = 1.0;
-	double ambient_temperature = 20;
-	double sensor_temperature = 20;
-	double water_temperature = 20;
-	double set_point_temperature = 60;
-
-	double sensor_time_constant = 1.0/300.0;
+	double sensor_temperature = param.ambient_temperature;
+	double water_temperature = param.ambient_temperature;
+	
 	double cooling_constant = 0.0001;
 
 	double integral = 0;
 	double derivative = 0;
-	double error, prev_error;
+	double error;
+	double prev_error = param.set_point_temperature - sensor_temperature;
 	double output;
 
 	int element_on = 0;
 
-	Kp = atol(argv[1]);
-	Ki = atol(argv[2]);
-	Kd = atol(argv[3]);
 
-	for (time = 0; time < 7200.0; time+=dt) {
+
+	for (time = param.t_start; time < param.t_end; time+=dt) {
 
 		//
 		// Update the model
@@ -49,32 +101,36 @@ int main (int argc, char ** argv) {
 
 		// Energy in from heating element
 		if (element_on) {
-			water_temperature += element_power * water_quantity * dt / SHC_H2O;
+			water_temperature += element_power * param.water_mass * dt / SHC_H2O;
 		}
 
 		// Energy out due to heat loss
-		water_temperature -= (water_temperature - ambient_temperature) * dt * cooling_constant;
+		water_temperature -= (water_temperature - param.ambient_temperature) * dt * cooling_constant;
 		if (water_temperature > 100.0) {
 			water_temperature = 100.0;
 		}
 
 		// Temperature sensor
-		sensor_temperature += (water_temperature - sensor_temperature) * dt * sensor_time_constant; // ?
+		sensor_temperature += (water_temperature - sensor_temperature) * dt * param.sensor_time_constant; // ?
 
 
-		// Update PID calculations.
-		error = set_point_temperature - sensor_temperature;
-		//error = set_point_temperature - water_temperature;
-		integral += error*dt;
-		derivative = (error-prev_error)/dt;
-		output = Kp * error + Ki * integral + Kd * derivative;
-		prev_error = error;
+		// Simulation iteration is every dt, but PID is evaluated less often.
+		if (i++ % 100 == 0) {
+			// Update PID calculations.
+			error = param.set_point_temperature - sensor_temperature;
+		
+			integral += error*dt*100;
+			derivative = (error-prev_error)/(dt*100);
+			output = param.Kp * error + param.Ki * integral + param.Kd * derivative;
+			prev_error = error;
 
-		// Heater element PWM
-		if (output <= 0 || time > 3600) {
-			element_on = 0;
-		} else {
-			element_on = ( (output/10.0) > fmod(time,100.0) ) ? 0 : 1;
+			// Heater element PWM
+			if (output <= 0) {
+			//if (output <= 0 || time > 3600) {
+				element_on = 0;
+			} else {
+				element_on = ( (output/10.0) >= fmod(time,300.0) ) ? 0 : 1;
+			}
 		}
 
 		fprintf (stdout, "%f %f %f   %f %f  %d\n", time, water_temperature, sensor_temperature, error, output, element_on);
