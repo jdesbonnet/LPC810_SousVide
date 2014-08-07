081bcc5ba4393d2abeee12a92690a37d0fff14d9
Joe Desbonnet <jdesbonnet@gmail.com>
2014-08-07 16:34:49 +0100

    Replace incorrect comment header

--- src/main.c
+++ src/main.c
@@ -1,29 +1,13 @@
 /****************************************************************************
- *   $Id:: lpc8xx_wkt.c 3635 2012-10-31 00:31:46Z usb00423                     $
- *   Project: NXP LPC8xx Wakeup timer example
+ *   Project: LPC810 Sous Vide cooking controller
  *
  *   Description:
- *     This file contains wakeup timer test modules.
+ *     A sous vide water bath temperature controller
  *
  ****************************************************************************
- * Software that is described herein is for illustrative purposes only
- * which provides customers with programming information regarding the
- * products. This software is supplied "AS IS" without any warranties.
- * NXP Semiconductors assumes no responsibility or liability for the
- * use of the software, conveys no license or title under any patent,
- * copyright, or mask work right to the product. NXP Semiconductors
- * reserves the right to make changes in the software without
- * notification. NXP Semiconductors also make no representation or
- * warranty that such application will be suitable for the specified
- * use without further testing or modification.
- 
- * Permission to use, copy, modify, and distribute this software and its 
- * documentation is hereby granted, under NXP Semiconductors' 
- * relevant copyright in the software, without fee, provided that it 
- * is used in conjunction with NXP Semiconductors microcontrollers. This 
- * copyright, permission, and disclaimer notice must appear in all copies of 
- * this code.
+ Originally based on NXP example code, but completely edited/changed.
 ****************************************************************************/
+
 #include "LPC8xx.h"			/* LPC8xx Peripheral Registers */
 #include "lpc8xx_gpio.h"
 
